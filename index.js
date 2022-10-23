0522561906717d0f9f354d7d508b4a1258201ea0
LiTallll <alistair.lewin@live.fr>
2022-10-23 10:55:28 +0200

    Dot Generation with direction and moving

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(function() {
     var used = []
     
     window.setInterval(function(){
-        if (turn%100==0) {
+        if (turn%50==0) {
             createDot(used);
         }
         console.log(used);
@@ -11,16 +11,39 @@ $(function() {
             moveADot(i);
         }
         turn++;
-    }, 100);
+    }, 10);
 });
 
 function createDot(used) {
     id = used.length
-    dir = ['u','r','d','l'];
-    $("#background").append("<div id='"+id+"' class='dot "+dir[1]+"'>");
+    dir = [['u',2,1],['r',0,2],['d',2,0],['l',1,2]];
+    var client = [document.getElementById('background').clientHeight, document.getElementById('background').clientWidth]
+    rngdir = dir[Math.floor(Math.random() * 4)];
+    $("#background").append("<div id='"+id+"' class='dot "+rngdir[0]+"'>");
+    if (rngdir[1]==2) {
+        $("#"+id).css({left: Math.random()*client[1]});
+    } else {
+        $("#"+id).css({left: rngdir[1]*client[1]});
+    }
+    if (rngdir[2]==2) {
+        $("#"+id).css({top: Math.random()*client[0]});
+    } else {
+        $("#"+id).css({top: rngdir[2]*client[0]});
+    }
     used.push(id);
 }
 
 function moveADot(id) {
-    $("#"+id).css({marginTop: '+=5px'});
+    if ($("#"+id).hasClass("u")) {
+        $("#"+id).css({top: '-=1px'});
+    }
+    if ($("#"+id).hasClass("r")) {
+        $("#"+id).css({left: '+=1px'});
+    }
+    if ($("#"+id).hasClass("d")) {
+        $("#"+id).css({top: '+=1px'});
+    }
+    if ($("#"+id).hasClass("l")) {
+        $("#"+id).css({left: '-=1px'});
+    }
 }
