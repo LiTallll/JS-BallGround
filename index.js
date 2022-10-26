5a34704a1c6daca30bb2eb325367bf15230baa8b
LiTallll <alistair.lewin@live.fr>
2022-10-26 23:59:52 +0200

    Delete Dot to reduce load

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
 $(function() {
     turn = 0
-    var used = []
     
     window.setInterval(function(){
         if (turn%50==0) {
-            createDot(used);
+            createDot();
         }
-        console.log(used);
-        for (i in used) {
-            moveADot(i);
-        }
+        $(".dot").each(function (e) {
+            moveADot(this);
+            DeleteDot(this);
+        });
         turn++;
     }, 10);
 });
 
-function createDot(used) {
-    id = used.length
+function createDot() {
     dir = [['u',2,1],['r',0,2],['d',2,0],['l',1,2]];
-    var client = [document.getElementById('background').clientHeight, document.getElementById('background').clientWidth]
+    var client = [document.getElementById('background').clientHeight, document.getElementById('background').clientWidth];
     rngdir = dir[Math.floor(Math.random() * 4)];
-    $("#background").append("<div id='"+id+"' class='dot "+rngdir[0]+"'>");
+    var divToAdd = $("<div class='dot "+rngdir[0]+"'>");
     if (rngdir[1]==2) {
-        $("#"+id).css({left: Math.random()*client[1]});
+        divToAdd.css({left: Math.floor(Math.random()*client[1])});
     } else {
-        $("#"+id).css({left: rngdir[1]*client[1]});
+        divToAdd.css({left: rngdir[1]*client[1]});
     }
     if (rngdir[2]==2) {
-        $("#"+id).css({top: Math.random()*client[0]});
+        divToAdd.css({top: Math.floor(Math.random()*client[0])});
     } else {
-        $("#"+id).css({top: rngdir[2]*client[0]});
+        divToAdd.css({top: rngdir[2]*client[0]});
     }
-    used.push(id);
+    $("#background").append(divToAdd);
+    console.log("Created");
 }
 
-function moveADot(id) {
-    if ($("#"+id).hasClass("u")) {
-        $("#"+id).css({top: '-=1px'});
+function moveADot(element) {
+    if ($(element).hasClass("u")) {
+        $(element).css({top: '-=1px'});
     }
-    if ($("#"+id).hasClass("r")) {
-        $("#"+id).css({left: '+=1px'});
+    if ($(element).hasClass("r")) {
+        $(element).css({left: '+=1px'});
     }
-    if ($("#"+id).hasClass("d")) {
-        $("#"+id).css({top: '+=1px'});
+    if ($(element).hasClass("d")) {
+        $(element).css({top: '+=1px'});
     }
-    if ($("#"+id).hasClass("l")) {
-        $("#"+id).css({left: '-=1px'});
+    if ($(element).hasClass("l")) {
+        $(element).css({left: '-=1px'});
+    }
+}
+
+function DeleteDot(element) {
+    pos = $(element).position();
+    var client = [document.getElementById('background').clientHeight, document.getElementById('background').clientWidth]
+    if (pos["top"] < 0 | pos["top"] > client[0]) {
+        console.log("Destroyed")
+        $(element).remove();
     }
 }
